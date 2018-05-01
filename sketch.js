02778597db640785b03e8ff27639e0857aa4c67a
Daniel <danieladriaenssen@gmail.com>
2018-05-01 14:15:12 +0200

    rød sirkel
    sirkel rød

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ function draw() {
 	//background(0, 255, 0); //255, 255, 255
 
 	// Sirkel
-	fill(255, 204, 0); // rød, grøn, blå
-	stroke('red');
+	fill(255, 0, 0); // rød, grøn, blå
+	//stroke('red');
 	strokeWeight(4);
 	ellipse(mouseX, mouseY, 55, 55);
 	//console.log(mouseX);
