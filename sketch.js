e352be75561012e4f224b9e31f3afec3951d9b32
Daniel <danieladriaenssen@gmail.com>
2018-05-01 14:12:11 +0200

    prøver igjenn
    rydder

--- sketch.js
+++ sketch.js
@@ -7,18 +7,12 @@ function setup() {
 function draw() {
 	//background(0, 255, 0); //255, 255, 255
 
-
-
 	// Sirkel
 	fill(255, 204, 0); // rød, grøn, blå
 	stroke('red');
 	strokeWeight(4);
 	ellipse(mouseX, mouseY, 55, 55);
 	//console.log(mouseX);
-
-	//Dette er endret nå
-	//Og dette er endret senere
-
 }
 
 function mousePressed() {
