4136cd627de5addc0e47f86f334cf2cb8c86bea7
Daniel <danieladriaenssen@gmail.com>
2018-05-01 13:03:02 +0200

    prøver vildt
    vet ikke hva jeg gjør

---
+++ spart.js
@@ -0,0 +1,11 @@
+// Rektangel
+fill(255, 204, 0,100); // rød, grøn, blå
+stroke('red');
+strokeWeight(4);
+rect(300,200,75,150);
+
+// Linje
+fill(255, 204, 0); // rød, grøn, blå
+stroke('red');
+strokeWeight(4);
+line(400,200,100,500);
