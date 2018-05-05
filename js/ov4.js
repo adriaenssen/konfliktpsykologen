e6f4feecb7935be75bc074c2215e9d58dd75aa1c
Daniel <danieladriaenssen@gmail.com>
2018-05-05 11:56:30 +0200

    med hjemmeside

--- js/ov4.js
+++ js/ov4.js
@@ -7,8 +7,9 @@ function draw() {
 	//fill(255, 0, 0); // rød, grøn, blå
 	//stroke('red');
 	background(0);
-	stroke(255);
-	strokeWeight(4);
+	//stroke(255);
+	//strokeWeight(4);
+	noStroke();
 	//noFill();
 	for(var x = 0; x <= width; x += 50){
 		for(var y =0; y <= height; y += 50){
